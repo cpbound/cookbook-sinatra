4387a5b313e480aee6c628cea8ef03e4a8c44d32
Christian Bound <automaticbound@gmail.com>
2022-07-04 13:42:11 +0900

    this is a sinatra app apparently

--- app.rb
+++ app.rb
@@ -8,8 +8,12 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-get "/:cookbook" do
-  puts params[:cookbook]
+require_relative "cookbook"
+require_relative "recipe"
+
+get "/" do
+  cookbook = Cookbook.new(File.join(__dir__, "recipes.csv"))
+  @recipes = cookbook.all
   erb :index
 end
 
@@ -20,4 +24,21 @@ end
 get "/team/:username" do
   puts params[:username]
   "the username is #{params[:username]}"
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/recipes" do
+  cookbook = Cookbook.new(File.join(__dir__, "recipes.csv"))
+  recipe = Recipe.new(params[:name], params[:description])
+  cookbook.add_recipe(recipe)
+  redirect to "/"
+end
+
+get "/recipes/:index" do
+  cookbook = Cookbook.new(File.join(__dir__, "recipes.csv"))
+  cookbook.remove_recipe(params[:index].to_i)
+  redirect to "/"
 end
